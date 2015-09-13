e58b2601c2008e791d95b11c9e51ea02e4fb1901
1sc0 <x1sc0@acrobotic.com>
2015-09-12 23:22:25 -0700

    adding activities 3 and 4

--- activity_02/sound.py
+++ activity_02/sound.py
@@ -3,10 +3,10 @@
   Author: Makerbro
   Platforms: Raspberry Pi (Raspbian)
   Language: Python
-  File: blink.py
+  File: sound.py
   ------------------------------------------------------------------------
   Description: 
-  Blinks an LED.
+  Uses PWM to generate sound from a piezoelectric transducer.
   ------------------------------------------------------------------------
   Please consider buying products from ACROBOTIC to help fund future
   Open-Source projects like this! We'll always put our best effort in every
@@ -21,17 +21,28 @@
 import RPi.GPIO as GPIO             # Import GPIO library
 import time                         # Import time library for sleep delay
 
-led_pin = 7                         # Pin number for LED
+buzz_pin = 11                       # Pin number for buzzer
 
 GPIO.setmode(GPIO.BOARD)            # Use board pin numbering
-GPIO.setup(led_pin, GPIO.OUT)       # Setup GPIO Pin 7 to OUT
-
+GPIO.setup(buzz_pin, GPIO.OUT)      # Setup GPIO Pin 7 to OUT
+delay_ms = 0.1                      # delay time in milliseconds for holding each duty
+                                    # cycle value
 try:
     while True:
-        GPIO.output(led_pin,True)   # Turn on GPIO pin 7
-        time.sleep(1)               # Hold the LED ON for 1 second
-        GPIO.output(led_pin,False)  # Turn on GPIO pin 7
-        time.sleep(1)               # Hold the LED ON for 1 second
+        # Loop through the frequency range ~100Hz to ~1.2KHz in increaments of 50
+        for i in range(100,1200,50):
+            pwm_obj = GPIO.PWM(buzz_pin,i)  # set pwm of buzz_pin to frequency "i"
+            pwm_obj.start(0)                # start the PWM with the duty cycle at 50%
+            # We want to make the sound increase in volume by adjusting the PWM duty cycle
+            # from 5% to 95%
+            for i in range(5,96,5):
+                pwm_obj.ChangeDutyCycle(i) 
+                time.sleep(delay_ms)
+            # We want to make the sound increase in volume by adjusting the PWM duty cycle
+            # from 95% to 5%
+            for i in range(95,4,-5):
+                pwm_obj.ChangeDutyCycle(i)
+                time.sleep(delay_ms)
 except KeyboardInterrupt:  
     # here you put any code you want to run before the program   
     # exits when you press CTRL+C  
